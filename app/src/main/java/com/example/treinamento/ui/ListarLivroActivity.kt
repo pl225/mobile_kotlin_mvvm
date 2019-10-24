6b4544fa05ac3562f5e64633fa9cb2893c8de808
Matheus Abreu <matheusa596@gmail.com>
2019-10-24 09:12:07 -0300

    Carregando lista de livros na tela principal.

--- app/src/main/java/com/example/treinamento/ui/ListarLivroActivity.kt
+++ app/src/main/java/com/example/treinamento/ui/ListarLivroActivity.kt
@@ -2,10 +2,12 @@ package com.example.treinamento.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.treinamento.R
+import kotlinx.android.synthetic.main.activity_listar_livro.*
 
 class ListarLivroActivity : AppCompatActivity() {
 
@@ -18,7 +20,8 @@ class ListarLivroActivity : AppCompatActivity() {
         viewModel =  ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(ListarLivroViewModel::class.java)
 
         viewModel.livros.observe(this, Observer {
-            Log.d("teste", it.size.toString())
+            rcyLivros.addItemDecoration(DividerItemDecoration(this, (rcyLivros.layoutManager as LinearLayoutManager).orientation))
+            rcyLivros.adapter = LivroAdapter(it)
         })
     }
 }
---
+++ app/src/main/java/com/example/treinamento/ui/LivroAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.treinamento.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.treinamento.R
+import com.example.treinamento.domain.model.Livro
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_livro.view.*
+
+class LivroAdapter(private val livros: List<Livro>): RecyclerView.Adapter<LivroAdapter.LivroViewHolder>() {
+
+    class LivroViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LivroViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_livro, parent, false)
+
+        return LivroViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return this.livros.size
+    }
+
+    override fun onBindViewHolder(holder: LivroViewHolder, position: Int) {
+        holder.itemView.txtTitulo.text = livros[position].titulo
+        holder.itemView.txtAutor.text = livros[position].autor
+        holder.itemView.txtPreco.text = livros[position].preco.toString()
+        Picasso
+            .get()
+            .load(livros[position].capa)
+            .into(holder.itemView.img)
+    }
+}
+
