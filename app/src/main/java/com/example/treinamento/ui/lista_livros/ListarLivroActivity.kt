c1b04657b39ee0faeb7a1521151110ae4ec16511
Matheus Abreu <matheusa596@gmail.com>
2019-10-28 11:02:10 -0300

    Adição de DrawerLayout.

--- app/src/main/java/com/example/treinamento/ui/lista_livros/ListarLivroActivity.kt
+++ app/src/main/java/com/example/treinamento/ui/lista_livros/ListarLivroActivity.kt
@@ -2,21 +2,30 @@ package com.example.treinamento.ui.lista_livros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.treinamento.R
+import com.example.treinamento.databinding.ActivityListarLivroBinding
 
 class ListarLivroActivity : AppCompatActivity() {
 
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_listar_livro)
+
+        val binding = DataBindingUtil.setContentView<ActivityListarLivroBinding>(this, R.layout.activity_listar_livro)
+        this.drawerLayout = binding.drawerLayout
 
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, this.drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return this.findNavController(R.id.myNavHostFragment).navigateUp()
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, this.drawerLayout)
     }
 }
